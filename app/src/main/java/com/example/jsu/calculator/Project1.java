1aa1440f084db3fe0fe0ce82fd35ed6d514dc475
ASantiago1014 <asantiago@stu.jsu.edu>
2019-02-14 14:06:05 -0600

    Added methods for numbers and Clear button

--- app/src/main/java/com/example/jsu/calculator/Project1.java
+++ app/src/main/java/com/example/jsu/calculator/Project1.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import android.widget.*;
+
 public class Project1 extends AppCompatActivity {
 
     @Override
@@ -26,6 +28,10 @@ public class Project1 extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.setText("0");
     }
 
     @Override
@@ -33,6 +39,166 @@ public class Project1 extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_project1, menu);
         return true;
+    }
+
+    public void oneButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("1");
+        }
+
+        else {
+
+            o.setText(o.getText() + "1");
+        }
+    }
+
+    public void twoButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("2");
+        }
+
+        else {
+
+            o.setText(o.getText() + "2");
+        }
+    }
+
+    public void threeButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("3");
+        }
+
+        else {
+
+            o.setText(o.getText() + "3");
+        }
+    }
+
+    public void fourButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("4");
+        }
+
+        else {
+
+            o.setText(o.getText() + "4");
+        }
+    }
+
+    public void fiveButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("5");
+        }
+
+        else {
+
+            o.setText(o.getText() + "5");
+        }
+    }
+
+    public void sixButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("6");
+        }
+
+        else {
+
+            o.setText(o.getText() + "6");
+        }
+    }
+
+    public void sevenButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("7");
+        }
+
+        else {
+
+            o.setText(o.getText() + "7");
+        }
+    }
+
+    public void eightButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("8");
+        }
+
+        else {
+
+            o.setText(o.getText() + "8");
+        }
+    }
+
+    public void nineButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            o.setText("9");
+        }
+
+        else {
+
+            o.setText(o.getText() + "9");
+        }
+    }
+
+    public void clearButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.setText("");
     }
 
     @Override
