06c7396cc471957c766a80dcf6ac24d4dcde0518
ASantiago1014 <asantiago@stu.jsu.edu>
2019-02-19 13:54:53 -0600

    Added Methods for most function buttons and Zero button

--- app/src/main/java/com/example/jsu/calculator/Project1.java
+++ app/src/main/java/com/example/jsu/calculator/Project1.java
@@ -10,8 +10,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import android.widget.*;
+import java.util.*;
 
 public class Project1 extends AppCompatActivity {
+
+    private double firstNumber;
+    private double secondNumber;
+    private double result;
+    private String value;
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -194,11 +201,198 @@ public class Project1 extends AppCompatActivity {
         }
     }
 
+    public void zeroButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.getText().toString();
+
+        if (o.getText() == "0") {
+
+            ;
+        }
+
+        else {
+
+            o.setText(o.getText() + "0");
+        }
+    }
+
     public void clearButtonClicked(View v) {
 
         TextView o = (TextView) findViewById(R.id.outputLabel);
 
         o.setText("");
+
+        firstNumber = 0;
+        secondNumber = 0;
+        result = 0;
+    }
+
+    public void equalButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        o.setText(String.valueOf(result));
+    }
+
+    public void signButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+        if (value == "" || value == "0")
+            ;
+
+
+        else {
+
+            if (value.contains(".")) {
+
+                double signChangeDouble = Double.parseDouble(value);
+
+                signChangeDouble = signChangeDouble * (-1);
+
+                o.setText(String.valueOf(signChangeDouble));
+            }
+
+            else {
+
+                int signChangeInt = Integer.parseInt(value);
+
+                signChangeInt = signChangeInt * (-1);
+
+                o.setText(String.valueOf(signChangeInt));
+
+            }
+        }
+    }
+
+    public void decimalButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+        if (value == "")
+            o.setText("0.");
+
+        else if (value.contains("."))
+            ;
+
+        else
+            o.setText(value + ".");
+
+    }
+
+    public void sqrtButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+        if (value == "" || value == "0")
+            ;
+
+        else {
+
+            double sqrtDouble = Double.parseDouble(value);
+
+            result = Math.sqrt(sqrtDouble);
+
+            o.setText(String.valueOf(result));
+        }
+
+    }
+
+    public void percentButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+
+    }
+
+    public void addButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+        if (o.getText() == "0" || o.getText() == "") {
+            ;
+        }
+
+        else {
+
+            firstNumber = Double.parseDouble(value);
+
+            o.setText("");
+
+
+        }
+    }
+
+    public void subButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+        if (o.getText() == "0" || o.getText() == "") {
+            ;
+        }
+
+        else {
+
+            firstNumber = Double.parseDouble(value);
+
+            o.setText("");
+
+
+        }
+    }
+
+    public void multiplyButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+        if (o.getText() == "0" || o.getText() == "") {
+            ;
+        }
+
+        else {
+
+            firstNumber = Double.parseDouble(value);
+
+            o.setText("");
+
+
+        }
+    }
+
+    public void divisionButtonClicked(View v) {
+
+        TextView o = (TextView) findViewById(R.id.outputLabel);
+
+        value = o.getText().toString();
+
+        if (o.getText() == "0" || o.getText() == "") {
+            ;
+        }
+
+        else {
+
+            firstNumber = Double.parseDouble(value);
+
+            o.setText("");
+
+
+        }
     }
 
     @Override
